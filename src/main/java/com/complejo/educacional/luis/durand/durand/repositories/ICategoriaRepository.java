5c689fae046d01107a20409e035a756eb227d4dd
Pablov <juan.vasquez@valposystem.com>
2023-08-04 22:46:22 -0400

    Creción de Repositori de Categoría

---
+++ src/main/java/com/complejo/educacional/luis/durand/durand/repositories/ICategoriaRepository.java
@@ -0,0 +1,29 @@
+package com.complejo.educacional.luis.durand.durand.repositories;
+
+import com.complejo.educacional.luis.durand.durand.models.Categoria;
+import com.complejo.educacional.luis.durand.durand.models.Pais;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+
+@Repository
+public interface ICategoriaRepository extends JpaRepository<Categoria, Long> {
+    // Seleccionar todas la instancias desde la BD
+    @Query(value = "select c from categoria c")
+    public List<Categoria> findAllCategoriaSort(Sort sort);
+
+    // Contar la cantidad de Paises que vienen en la búsqueda y páginarlos
+    @Query(value = "select p from categoria c",
+    countQuery = "select count(p) from categoria p")
+    public Page<Categoria> findAllCategoriaPage(Pageable pageable);
+
+    //Buscar por id
+    @Query(value = "select c from categoria c = where id=:id")
+    public Pais findByIdPais(long id);
+}
