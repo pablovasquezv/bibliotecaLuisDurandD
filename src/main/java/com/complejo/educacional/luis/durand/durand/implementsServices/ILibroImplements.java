526d3fcfa09583dea9dddd756a929e0e1fad02d5
Pablov <juan.vasquez@valposystem.com>
2023-08-07 20:56:44 -0400

    Creación de interface ILibroImplements.java

---
+++ src/main/java/com/complejo/educacional/luis/durand/durand/implementsServices/ILibroImplements.java
@@ -0,0 +1,13 @@
+package com.complejo.educacional.luis.durand.durand.implementsServices;
+
+import com.complejo.educacional.luis.durand.durand.models.Libro;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public interface ILibroImplements {
+
+
+}
