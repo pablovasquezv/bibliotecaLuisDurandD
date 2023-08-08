6f72c5f54587ceb1e4e56b96de1bc321de3aedfd
Pablov <juan.vasquez@valposystem.com>
2023-08-07 20:59:49 -0400

    Creación de métodos para interface ILibroImplements.java

--- src/main/java/com/complejo/educacional/luis/durand/durand/implementsServices/ILibroImplements.java
+++ src/main/java/com/complejo/educacional/luis/durand/durand/implementsServices/ILibroImplements.java
@@ -7,7 +7,58 @@ import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
+/**
+ * @author Pablo
+ */
 public interface ILibroImplements {
+    /**
+     *
+     * @param libro
+     * @return
+     * @throws Exception
+     */
+    public Libro createLibro( Libro libro)throws Exception;
+
+    /**
+     *
+     * @param id
+     * @param libro
+     * @return
+     * @throws Exception
+     */
+    public Libro udateLibro(Long id, Libro libro) throws Exception;
+
+    /**
+     *
+     * @param sort
+     * @return
+     * @throws Exception
+     */
+    public List<Libro> findAllLibroSort(Sort sort)throws Exception;
+
+    /**
+     *
+     * @param pageable
+     * @return
+     * @throws Exception
+     */
+    public Page<Libro> findAllLibroPage(Pageable pageable) throws Exception;
+
+    /**
+     *
+     * @param id
+     * @return
+     * @throws Exception
+     */
+    public Libro findAllIdLibro(long id)throws Exception;
+
+    /**
+     *
+     * @param id
+     * @return
+     * @throws Exception
+     */
+    public Libro deleteLibroById(Long id)throws Exception;
 
 
 }
