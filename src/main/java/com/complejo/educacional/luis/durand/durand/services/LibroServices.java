3037773e56705a67bb43c5f26cb12062efb2f4c4
Pablov <juan.vasquez@valposystem.com>
2023-08-07 21:03:53 -0400

    Implementación de métodos de interface ILibroImplements.java en clase LibroServices.java

--- src/main/java/com/complejo/educacional/luis/durand/durand/services/LibroServices.java
+++ src/main/java/com/complejo/educacional/luis/durand/durand/services/LibroServices.java
@@ -1,7 +1,44 @@
 package com.complejo.educacional.luis.durand.durand.services;
 
+import com.complejo.educacional.luis.durand.durand.implementsServices.ILibroImplements;
+import com.complejo.educacional.luis.durand.durand.models.Libro;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
 /**
  * @author Pablo
  */
-public class LibroServices {
+public class LibroServices implements ILibroImplements {
+    @Override
+    public Libro createLibro(Libro libro) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Libro udateLibro(Long id, Libro libro) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Libro> findAllLibroSort(Sort sort) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Page<Libro> findAllLibroPage(Pageable pageable) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Libro findAllIdLibro(long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Libro deleteLibroById(Long id) throws Exception {
+        return null;
+    }
 }
