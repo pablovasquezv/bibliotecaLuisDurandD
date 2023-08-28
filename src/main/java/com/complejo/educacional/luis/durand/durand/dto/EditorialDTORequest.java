fb3536326cdd1fae939f35e184ada7471c1d47b1
Pablov <juan.vasquez@valposystem.com>
2023-08-28 17:48:39 -0400

    Creación de clase EditorialDTORequest.java con sus atributos

---
+++ src/main/java/com/complejo/educacional/luis/durand/durand/dto/EditorialDTORequest.java
@@ -0,0 +1,22 @@
+package com.complejo.educacional.luis.durand.durand.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author Pablo
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class EditorialDTORequest {
+    //Atributos
+    private Long id_editorial;
+    private String nombre_editorial;
+    private String descripcion_editorial;
+    private String direccion_editorial;
+    private String telefono_editorial;
+    private String correoElectronico_editorial;
+}
