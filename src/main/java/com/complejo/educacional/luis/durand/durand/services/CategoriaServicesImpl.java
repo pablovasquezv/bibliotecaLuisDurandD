61190289a45178639ba2fcb0c3f719306fb18456
Pablov <juan.vasquez@valposystem.com>
2023-08-29 23:16:16 -0400

    Creación de la lógica de los métodos

--- src/main/java/com/complejo/educacional/luis/durand/durand/services/CategoriaServicesImpl.java
+++ src/main/java/com/complejo/educacional/luis/durand/durand/services/CategoriaServicesImpl.java
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,7 +29,6 @@ public class CategoriaServicesImpl implements ICategoriaServices {
     private ObjectMapper objectMapper;
 
     /**
-     *
      * @param categoriaDTORequest
      * @return categoriaDTORequest
      * @throws Exception
@@ -39,7 +39,7 @@ public class CategoriaServicesImpl implements ICategoriaServices {
     public CategoriaDTORequest saveCategoria(CategoriaDTORequest categoriaDTORequest) throws Exception {
 
         try {
-            Categoria categoriaCreate= new Categoria(
+            Categoria categoriaCreate = new Categoria(
                     null,
                     categoriaDTORequest.getNombre_categoria(),
                     categoriaDTORequest.getDescripcion_categoria(),
@@ -50,18 +50,24 @@ public class CategoriaServicesImpl implements ICategoriaServices {
             iCategoriaRepository.save(categoriaCreate);
             log.info("Categoría creda", objectMapper.writeValueAsString(categoriaDTORequest));
             return categoriaDTORequest;
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("Falló la creación de la Categoria =>", e.getCause().toString());
         }
         return categoriaDTORequest;
     }
 
+    /**
+     * @param id
+     * @param categoriaRequestUpdate
+     * @return categoriaDTOResponse
+     * @throws Exception
+     */
     @Override
     @Transactional(readOnly = false)
-    public CategoriaDTOResponse updateCategoria(CategoriaRequestUpdate categoriaRequestUpdate) throws Exception {
-        Optional<Categoria> optionalCategoria=null;
-        EditorialDTOResponse editorialDTOResponse= null;
-        Categoria categoriaUpdate= new Categoria(
+    public CategoriaDTOResponse updateCategoria(Long id, CategoriaRequestUpdate categoriaRequestUpdate) throws Exception {
+        Optional<Categoria> optionalCategoria = null;
+        CategoriaDTOResponse categoriaDTOResponse = null;
+        Categoria categoriaUpdate = new Categoria(
                 categoriaRequestUpdate.getId_categoria(),
                 categoriaRequestUpdate.getNombre_categoria(),
                 categoriaRequestUpdate.getDescripcion_categoria(),
@@ -69,34 +75,111 @@ public class CategoriaServicesImpl implements ICategoriaServices {
                 categoriaRequestUpdate.getUdpdateAt()
         );
         try {
-optionalCategoria=iCategoriaRepository.findById()
-        }catch (Exception e){
+            optionalCategoria = iCategoriaRepository.findById(id);
+            if (optionalCategoria.isPresent()) {
+                categoriaUpdate = iCategoriaRepository.save(categoriaUpdate);
+                log.info("Categoría Actualizada!", objectMapper.writeValueAsString(categoriaRequestUpdate));
+            } else {
+                log.error("Falló la actualización de la Categoría =>");
+            }
+            categoriaDTOResponse = new CategoriaDTOResponse(
+                    categoriaUpdate.getId_categoria(),
+                    categoriaUpdate.getNombre_categoria(),
+                    categoriaUpdate.getDescripcion_categoria(),
+                    categoriaUpdate.getCreateAt(),
+                    categoriaUpdate.getUdpdateAt()
+            );
+            return categoriaDTOResponse;
+        } catch (Exception e) {
             log.error("Falló la creación de la Categoria =>", e.getCause().toString());
         }
-        return null;
+        return categoriaDTOResponse;
     }
 
+    /**
+     * @param sort
+     * @return categoriaDTOResponses
+     * @throws Exception
+     */
     @Override
     @Transactional(readOnly = true)
     public List<CategoriaDTOResponse> findAllCategoriaSort(Sort sort) throws Exception {
-        return null;
+        List<CategoriaDTOResponse> categoriaDTOResponses = new ArrayList<CategoriaDTOResponse>();
+        for (Categoria categoria : iCategoriaRepository.findAllCategoriaSort(sort)) {
+            categoriaDTOResponses.add(
+                    new CategoriaDTOResponse(
+                            categoria.getId_categoria(),
+                            categoria.getNombre_categoria(),
+                            categoria.getDescripcion_categoria(),
+                            categoria.getCreateAt(),
+                            categoria.getUdpdateAt()
+                    )
+            );
+        }
+        return categoriaDTOResponses;
     }
 
+    /**
+     *
+     * @param pageable
+     * @return categoriaDTOResponses
+     * @throws Exception
+     */
     @Override
     @Transactional(readOnly = true)
     public Page<CategoriaDTOResponse> findAllCategoriaPage(Pageable pageable) throws Exception {
-        return null;
+        List<CategoriaDTOResponse> categoriaDTOResponses = new ArrayList<CategoriaDTOResponse>();
+        for (Categoria categoria : iCategoriaRepository.findAllCategoriaPage(pageable)) {
+            categoriaDTOResponses.add(
+                    new CategoriaDTOResponse(
+                            categoria.getId_categoria(),
+                            categoria.getNombre_categoria(),
+                            categoria.getDescripcion_categoria(),
+                            categoria.getCreateAt(),
+                            categoria.getUdpdateAt()
+                    )
+            );
+        }
+        return (Page<CategoriaDTOResponse>) categoriaDTOResponses;
     }
 
+    /**
+     *
+     * @param id
+     * @return CategoriaDTOResponse
+     * @throws Exception
+     */
     @Override
     @Transactional(readOnly = true)
     public CategoriaDTOResponse findByIdCategoria(long id) throws Exception {
-        return null;
+        Categoria categoriaId = iCategoriaRepository.findByIdCategoria(id);
+        if (categoriaId == null) {
+            throw new Exception("¡Categoría not found!");
+        }
+        return new CategoriaDTOResponse(
+                categoriaId.getId_categoria(),
+                categoriaId.getNombre_categoria(),
+                categoriaId.getDescripcion_categoria(),
+                categoriaId.getCreateAt(),
+                categoriaId.getUdpdateAt()
+        );
     }
 
+    /**
+     *
+     * @param id
+     * @return eliminarCategoria
+     * @throws Exception
+     */
     @Override
     @Transactional(readOnly = false)
     public Object deleteCategoriaById(Long id) throws Exception {
-        return null;
+        Object eliminarCategoria= null;
+        try {
+            log.info("¡Eliminar Categoría");
+        }catch (Exception e){
+            log.error("¡Falló la eliminación de la Categoría"+e.getCause().toString());
+        }
+        return eliminarCategoria;
     }
 }
