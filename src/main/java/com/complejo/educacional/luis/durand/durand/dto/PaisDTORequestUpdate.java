5dfa58dd1fb61cf2b5e571218d652041a832e56b
Pablov <juan.vasquez@valposystem.com>
2023-08-30 14:25:09 -0400

    Creación de atributos para las clases DTO

--- src/main/java/com/complejo/educacional/luis/durand/durand/dto/PaisDTORequestUpdate.java
+++ src/main/java/com/complejo/educacional/luis/durand/durand/dto/PaisDTORequestUpdate.java
@@ -1,4 +1,28 @@
 package com.complejo.educacional.luis.durand.durand.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+/**
+ * @author Pablo
+ * @Data: Para crear los gett y sett
+ * @AllArgsConstructor: Constructor con parámetros
+ * @NoArgsConstructor:Constructor sin parámetros
+ * @ToString: para obtener todos los datos
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class PaisDTORequestUpdate {
+    //Atributos
+    private Long id_pais;
+    private String nombre_pais;
+    private Date createdAt;
+    private Date updatedAt;
 }
