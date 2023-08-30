f2dce7ecf762d05b84303ed0f46382e4566169cf
Pablov <juan.vasquez@valposystem.com>
2023-08-29 21:45:59 -0400

    Creación de Cases DTO con sus atributos

---
+++ src/main/java/com/complejo/educacional/luis/durand/durand/dto/CategoriaDTORequest.java
@@ -0,0 +1,28 @@
+package com.complejo.educacional.luis.durand.durand.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+/**
+ * @author Pablo
+ * @Data: Para crear los gett y sett
+ * @AllArgsConstructor: Constructor con parámetros
+ * @NoArgsConstructor:Constructor sin parámetros
+ * @ToString para obtener los datos
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CategoriaDTORequest {
+    //Atributos
+    private String nombre_categoria;
+    private String descripcion_categoria;
+    private Date createAt;
+    private Date udpdateAt;
+}
