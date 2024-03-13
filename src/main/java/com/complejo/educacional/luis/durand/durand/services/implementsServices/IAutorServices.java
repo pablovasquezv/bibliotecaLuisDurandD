cad00baaa4344549d63555943416df2fd9adcb9c
Pablov <juan.vasquez@valposystem.com>
2024-03-13 01:55:38 -0300

    Limpieza de importación que no se utiliza

--- src/main/java/com/complejo/educacional/luis/durand/durand/services/implementsServices/IAutorServices.java
+++ src/main/java/com/complejo/educacional/luis/durand/durand/services/implementsServices/IAutorServices.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
-import com.complejo.educacional.luis.durand.durand.models.Autor;
 
 /**
  * @author Pablo
