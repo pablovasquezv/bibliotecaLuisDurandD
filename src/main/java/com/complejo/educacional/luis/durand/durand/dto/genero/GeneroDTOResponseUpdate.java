a1f796694a0f34316bd4de3e5c219cc52549ca8d
Pablov <juan.vasquez@valposystem.com>
2024-03-30 01:22:09 -0300

    Creación de clase GeneroDTOResponseUpdate.java.

---
+++ src/main/java/com/complejo/educacional/luis/durand/durand/dto/genero/GeneroDTOResponseUpdate.java
@@ -0,0 +1,10 @@
+package com.complejo.educacional.luis.durand.durand.dto.genero;
+
+/**
+ * @author Pablo
+ * @version 1.0
+ * @create 30-03-2024 1:21
+ * @project bibliotecaLuisDurandD
+ */
+public class GeneroDTOResponseUpdate {
+}
