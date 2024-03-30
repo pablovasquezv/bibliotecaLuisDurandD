e4090ec38dd76124fb67695abc41f3056adac1d4
Pablov <juan.vasquez@valposystem.com>
2024-03-30 01:24:45 -0300

    Creación de atributos y implementación anotaciones Spring boot.

--- src/main/java/com/complejo/educacional/luis/durand/durand/dto/genero/GeneroDTOResponseUpdate.java
+++ src/main/java/com/complejo/educacional/luis/durand/durand/dto/genero/GeneroDTOResponseUpdate.java
@@ -1,4 +1,27 @@
 package com.complejo.educacional.luis.durand.durand.dto.genero;
+//Import necesarios para la clase.
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+/**
+ * @Setter: Para crear los métodos sett.
+ * @Getter: Para crear los métodos gett.
+ * @AllArgsConstructor: Constructor con parámetros.
+ * @NoArgsConstructor:Constructor sin parámetros.
+ * @ToString para obtener los datos.
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 
 /**
  * @author Pablo
@@ -7,4 +30,11 @@ package com.complejo.educacional.luis.durand.durand.dto.genero;
  * @project bibliotecaLuisDurandD
  */
 public class GeneroDTOResponseUpdate {
+    //Atributos
+    private Long id_genero;
+    private String nombre_genero;
+    private String descripcion_genero;
+    private Long id_pais;
+    private Date createdAt;
+    private Date updatedAt;
 }
