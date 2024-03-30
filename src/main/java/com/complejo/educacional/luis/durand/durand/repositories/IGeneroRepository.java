b7f3e529f0b4b4c0b8c3508c03069983a5b42936
Pablov <juan.vasquez@valposystem.com>
2024-03-29 23:35:59 -0300

    Creación de interface IGeneroRepository

---
+++ src/main/java/com/complejo/educacional/luis/durand/durand/repositories/IGeneroRepository.java
@@ -0,0 +1,14 @@
+package com.complejo.educacional.luis.durand.durand.repositories;
+
+import com.complejo.educacional.luis.durand.durand.models.Genero;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author Pablo
+ * @version 1.0
+ * @create 29-03-2024 23:34
+ * @project bibliotecaLuisDurandD
+ */
+public interface IGeneroRepository extends JpaRepository<Genero, Long> {
+
+}
