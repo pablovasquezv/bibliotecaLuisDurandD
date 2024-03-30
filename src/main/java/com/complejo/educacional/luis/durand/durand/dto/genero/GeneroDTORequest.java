b1e8aaebaea69f4fda44e4aa1b4594e75c7b4c81
Pablov <juan.vasquez@valposystem.com>
2024-03-30 01:11:13 -0300

    Creación de atributos y implementación anotaciones SprintBoot.

--- src/main/java/com/complejo/educacional/luis/durand/durand/dto/genero/GeneroDTORequest.java
+++ src/main/java/com/complejo/educacional/luis/durand/durand/dto/genero/GeneroDTORequest.java
@@ -1,4 +1,26 @@
 package com.complejo.educacional.luis.durand.durand.dto.genero;
+//Import necesarias para la clase.
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+/**
+ * @Setter: Para crear los métodos sett.
+ * @Getter: Para crear los métodos gett.
+ * @AllArgsConstructor: Constructor con parámetros.
+ * @NoArgsConstructor:Constructor sin parámetros.
+ * @ToString para obtener los datos.
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 
 /**
  * @author Pablo
@@ -6,5 +28,12 @@ package com.complejo.educacional.luis.durand.durand.dto.genero;
  * @create 30-03-2024 0:42
  * @project bibliotecaLuisDurandD
  */
+
 public class GeneroDTORequest {
+    //Atributos
+    private String nombre_genero;
+    private String descripcion_genero;
+    private Long id_pais;
+    private Date createdAt;
+    private Date updatedAt;
 }
