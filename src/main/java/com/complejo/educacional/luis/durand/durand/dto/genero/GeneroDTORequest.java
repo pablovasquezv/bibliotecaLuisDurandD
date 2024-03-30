954c7cc79c869805baa697d8f13c4d5c65c7eddd
Pablov <juan.vasquez@valposystem.com>
2024-03-30 00:44:12 -0300

    Creación de clase GeneroDTORequest.java y paquete.

---
+++ src/main/java/com/complejo/educacional/luis/durand/durand/dto/genero/GeneroDTORequest.java
@@ -0,0 +1,10 @@
+package com.complejo.educacional.luis.durand.durand.dto.genero;
+
+/**
+ * @author Pablo
+ * @version 1.0
+ * @create 30-03-2024 0:42
+ * @project bibliotecaLuisDurandD
+ */
+public class GeneroDTORequest {
+}
