125dc8c66e2f5fee701b2ac5fa7d70a17dc506ad
Pablov <juan.vasquez@valposystem.com>
2024-03-29 23:52:41 -0300

    Creación de consultas JPQL en interface IGeneroRepository

--- src/main/java/com/complejo/educacional/luis/durand/durand/repositories/IGeneroRepository.java
+++ src/main/java/com/complejo/educacional/luis/durand/durand/repositories/IGeneroRepository.java
@@ -1,7 +1,13 @@
 package com.complejo.educacional.luis.durand.durand.repositories;
 
 import com.complejo.educacional.luis.durand.durand.models.Genero;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * @author Pablo
@@ -10,5 +16,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @project bibliotecaLuisDurandD
  */
 public interface IGeneroRepository extends JpaRepository<Genero, Long> {
+
+    //Seleccionar todas la instancias desde Genero la BD.
+    @Query(value = "select g from Genero c")
+    public List<Genero> findAllGeneroSort(Sort sort);
+
+    //Contar la cantidad Géneros que vienen en la búsqueda y páginarlos.
+    @Query(value = "select g from Genero g")
+    public Page<Genero> findAllGeneroPage(Pageable pageable);
+
+    //Buscar por ID.
+    @Query(value = "select g from Genero g where id=:id")
+    public Genero findByIdCategoria(long id );
+
 
 }
