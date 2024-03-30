40c5d77ebe30e220edd3d6f4e2e35f5020761933
Pablov <juan.vasquez@valposystem.com>
2024-03-30 19:57:54 -0300

    Creación de clase controlador GeneroRestController.java.

---
+++ src/main/java/com/complejo/educacional/luis/durand/durand/controllers/GeneroRestController.java
@@ -0,0 +1,10 @@
+package com.complejo.educacional.luis.durand.durand.controllers;
+
+/**
+ * @author Pablo
+ * @version 1.0
+ * @create 30-03-2024 19:57
+ * @project bibliotecaLuisDurandD
+ */
+public class GeneroRestController {
+}
