518fb7cafe1ca804791ac5a3c0f5534bdc4b2be3
Pablov <juan.vasquez@valposystem.com>
2024-03-30 19:18:00 -0300

    Creación de interfaz IGeneroServices.java

---
+++ src/main/java/com/complejo/educacional/luis/durand/durand/services/implementsServices/IGeneroServices.java
@@ -0,0 +1,14 @@
+package com.complejo.educacional.luis.durand.durand.services.implementsServices;
+
+
+/**
+ * @author Pablo
+ * @version 1.0
+ * @create 30-03-2024 1:25
+ * @project bibliotecaLuisDurandD
+ */
+public interface IGeneroServices {
+
+
+
+}
