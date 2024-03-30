22a840efcf605a8562970a30397833427e1bc7bb
Pablov <juan.vasquez@valposystem.com>
2024-03-29 23:51:49 -0300

    Agregar comentarios en ICategoriaRepository.java

--- src/main/java/com/complejo/educacional/luis/durand/durand/repositories/ICategoriaRepository.java
+++ src/main/java/com/complejo/educacional/luis/durand/durand/repositories/ICategoriaRepository.java
@@ -11,18 +11,24 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * @author Pablo
+ * @version 1.0
+ * @create 05-08-2023 23:34
+ * @project bibliotecaLuisDurandD
+ */
 @Repository
 public interface ICategoriaRepository extends JpaRepository<Categoria, Long> {
-    // Seleccionar todas la instancias desde la BD
+    // Seleccionar todas la instancias desde Categoria la BD.
     @Query(value = "select c from Categoria c")
     public List<Categoria> findAllCategoriaSort(Sort sort);
 
-    // Contar la cantidad de Paises que vienen en la búsqueda y páginarlos
+    // Contar la cantidad de Categoria que vienen en la búsqueda y páginarlos.
     @Query(value = "select c from Categoria c",
     countQuery = "select count(c) from Categoria c")
     public Page<Categoria> findAllCategoriaPage(Pageable pageable);
 
-    //Buscar por id
+    //Buscar por id.
     @Query(value = "select c from Categoria c where id=:id")
     public Categoria findByIdCategoria(long id);
 
