50787c9743d8691c36d327ff231f67fedf0c2135
Pablov <juan.vasquez@valposystem.com>
2024-03-30 19:26:31 -0300

    Creación de métodos.

--- src/main/java/com/complejo/educacional/luis/durand/durand/services/implementsServices/IGeneroServices.java
+++ src/main/java/com/complejo/educacional/luis/durand/durand/services/implementsServices/IGeneroServices.java
@@ -1,5 +1,13 @@
 package com.complejo.educacional.luis.durand.durand.services.implementsServices;
+//Import necesarias para la clase.
+import com.complejo.educacional.luis.durand.durand.dto.genero.GeneroDTORequest;
+import com.complejo.educacional.luis.durand.durand.dto.genero.GeneroDTOResponse;
+import com.complejo.educacional.luis.durand.durand.dto.genero.GeneroDTOResponseUpdate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.List;
 
 /**
  * @author Pablo
@@ -9,6 +17,47 @@ package com.complejo.educacional.luis.durand.durand.services.implementsServices;
  */
 public interface IGeneroServices {
 
+    /**
+     * @param generoDTORequest
+     * @return
+     * @throws Exception
+     */
+    public GeneroDTORequest saveGenero(GeneroDTORequest generoDTORequest) throws Exception;
 
+    /**
+     * @param id
+     * @param generoDTOResponseUpdate
+     * @return
+     * @throws Exception
+     */
+    public GeneroDTOResponseUpdate updatGenero(Long id, GeneroDTOResponseUpdate generoDTOResponseUpdate) throws Exception;
+
+    /**
+     * @param sort
+     * @return
+     * @throws Exception
+     */
+    public List<GeneroDTOResponse> findAllGeneroSort(Sort sort) throws Exception;
+
+    /**
+     * @param pageable
+     * @return
+     * @throws Exception
+     */
+    public Page<GeneroDTOResponse> findAllGeneroPage(Pageable pageable) throws Exception;
+
+    /**
+     * @param id
+     * @return
+     * @throws Exception
+     */
+    public GeneroDTOResponse findByIdGenero(long id) throws Exception;
+
+    /**
+     * @param id
+     * @return
+     * @throws Exception
+     */
+    public Object deleteGeneroById(Long id) throws Exception;
 
 }
