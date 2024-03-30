3c8b191141da059825037bc2af5f8ea9eb46d27f
Pablov <juan.vasquez@valposystem.com>
2024-03-29 23:26:28 -0300

    Crear la clase Genero.java

---
+++ src/main/java/com/complejo/educacional/luis/durand/durand/models/Genero.java
@@ -0,0 +1,10 @@
+package com.complejo.educacional.luis.durand.durand.models;
+
+/**
+ * @author Pablo
+ * @version 1.0
+ * @create 29-03-2024 23:24
+ * @project bibliotecaLuisDurandD
+ */
+public class Genero {
+}
