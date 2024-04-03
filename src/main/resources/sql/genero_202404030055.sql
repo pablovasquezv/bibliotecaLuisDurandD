15a8c7bbc110d153d4f7479453a857bafbd1b99e
Pablov <juan.vasquez@valposystem.com>
2024-04-03 01:14:06 -0300

    Agregar sql de géneros.

---
+++ src/main/resources/sql/genero_202404030055.sql
@@ -0,0 +1,13 @@
+INSERT INTO public.genero (created_at,descripcion_genero,nombre_genero,updated_at) VALUES
+	 ('2024-03-31 02:38:57.155','La poesía expresa sentimientos, tratando que el lector se sienta atraídas por ellas.','Poesía','2024-03-31 00:34:22.134'),
+	 ('2024-03-31 02:48:21.912','La poesía expresa sentimientos, tratando que el lector se sienta atraídas por ellas.','POESÍA','2024-03-31 00:34:22.134'),
+	 ('2024-04-02 20:06:37.128','Este género busca desafiar los miedos más profundos del ser humano, explorando temas como la muerte, el mal, la locura y lo paranormal.','TERROR','2024-04-02 19:27:49.966'),
+	 ('2024-04-03 00:07:07.45','Un género literario que mantiene al lector en vilo con giros inesperados y una atmósfera intensa.','Suspenso psicológico','2024-04-03 00:22:58.706'),
+	 ('2024-04-03 00:27:08.109','Emocionantes historias que llevan al lector a vivir experiencias extraordinarias y peligrosas.','AVENTURAS','2024-03-31 00:34:22.134'),
+	 ('2024-04-03 00:33:07.807','Explora mundos imaginarios, futuristas y tecnológicos, a menudo con elementos científicos especulativos.','CIENCIA FICCIÓN','2024-04-03 00:29:52.605'),
+	 ('2024-04-03 00:33:47.405','Libros que tratan temas reales, como filosofía, historia, biografías, ensayos, divulgación científica, entre otros.','NO FICCIÓN','2024-04-03 00:29:52.605'),
+	 ('2024-03-31 02:46:55.089',' Ambientado en un período histórico específico, con eventos, costumbres y personajes de esa época.','HISTÓRICO','2024-04-03 00:44:41.855'),
+	 ('2024-04-03 00:45:58.606','Se centra en las relaciones amorosas y emocionales entre los personajes.','ROMANCE','2024-04-03 00:45:42.612'),
+	 ('2024-04-03 00:46:23.895','Presenta enigmas, intrigas y situaciones desconcertantes que mantienen al lector en suspenso.','MISTERIO','2024-04-03 00:45:42.612');
+INSERT INTO public.genero (created_at,descripcion_genero,nombre_genero,updated_at) VALUES
+	 ('2024-04-03 00:46:55.622','Busca provocar la risa y el entretenimiento a través de situaciones cómicas y personajes extravagantes.','HUMOR','2024-04-03 00:45:42.612');
