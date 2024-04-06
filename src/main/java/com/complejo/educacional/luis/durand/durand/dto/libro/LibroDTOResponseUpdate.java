5a7c1ff2f93644812aa3454c66bab05ac912f6ae
Pablov <juan.vasquez@valposystem.com>
2024-04-06 15:28:13 -0300

    Creación de clases DTO.

---
+++ src/main/java/com/complejo/educacional/luis/durand/durand/dto/libro/LibroDTOResponseUpdate.java
@@ -0,0 +1,44 @@
+package com.complejo.educacional.luis.durand.durand.dto.libro;
+
+import com.complejo.educacional.luis.durand.durand.models.Autor;
+import com.complejo.educacional.luis.durand.durand.models.Categoria;
+import com.complejo.educacional.luis.durand.durand.models.Editorial;
+import com.complejo.educacional.luis.durand.durand.models.Genero;
+//Import necesarias para la clase.
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+/**
+ * @Setter: Para crear los métodos sett.
+ * @Getter: Para crear los métodos gett.
+ * @AllArgsConstructor: Constructor con parámetros.
+ * @NoArgsConstructor:Constructor sin parámetros.
+ * @ToString para obtener los datos.
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+/**
+ * @author Pablo
+ * @version 1.0
+ * @create 06-04-2024 14:23
+ * @project bibliotecaLuisDurandD
+ */
+public class LibroDTOResponseUpdate {
+    private Long id_libro;
+    private String titulo_libro;
+    private Autor autor;
+    private Categoria categoria;
+    private Editorial editorial;
+    private Genero genero;
+    private Integer edicion_libro;
+    private int paginas_libro;
+}
