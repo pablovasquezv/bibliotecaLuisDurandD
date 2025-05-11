8b1eb10009a9a0c1683b54f57cebed270484a585
Pablo <pablo1986vasquez@gmail.com>
2025-05-10 23:51:01 -0400

    Crear clase DTO.

---
+++ src/main/java/com/complejo/educacional/luis/durand/durand/dto/prestamo/PrestamoDTORequest.java
@@ -0,0 +1,32 @@
+package com.complejo.educacional.luis.durand.durand.dto.prestamo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//@ToString(exclude = "autoresIds") // Si quieres evitar imprimir la lista completa en logs
+@Builder
+/**
+ * @version 1.0
+ * @autor Pablo
+ * @creates 10-05-2025 23:45
+ * @proyect bibliotecaLuisDurandD
+ */
+public class PrestamoDTORequest {
+    private Long id_usuario;
+    private Long id_libro;
+    private LocalDate fechaPrestamo;
+    private BigDecimal multa;
+    private String observaciones;
+    private Date createdAt;
+    private Date updatedAt;
+}
